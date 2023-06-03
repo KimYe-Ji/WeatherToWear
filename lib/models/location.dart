25829e9128cf42bc38296d4646f5e9677d4ab200
KimYe-Ji <dywjd0624@gmail.com>
2023-06-04 05:06:44 +0900

    position 말고 위도, 경도로 넘길 수 있게 수정

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -5,11 +5,21 @@ import 'dart:convert';
 
 class Location {
   // late: 값의 초기화를 뒤로 미룸 + null 사용 방지
-  late Position position;
+  //late Position position;
+  late double llat, llon;
   late String address;
   late List<Weather> tempList;
   late List<Weather> popList;
   late List<Weather> rehList;
+  late List<Weather> skyList;
+
+  // 현재 시간에 따른 날씨 정보 string 형태의 수치
+  // index 0 - 현재 온도
+  // index 1 - 강수 확률
+  // index 2 - 습도 
+  // index 3 - 하늘 맑은 정도 
+  late List<String> weatherNowList;
+
   late double tmn = -273.15;
   late double tmx = 1200;
 
@@ -19,16 +29,27 @@ class Location {
     tempList = new List.empty(growable: true);
     popList = new List.empty(growable: true);
     rehList = new List.empty(growable: true);
+    skyList = new List.empty(growable: true);
+    weatherNowList = new List.empty(growable: true);
   }
 
   // set location's long, lat
+  /*
   void setPosition(Position position) {
     this.position = position;
 
     print(position);
   }
+*/
+  void setPosition2(double lat, double lon) {
+    this.llat = lat;
+    this.llon = lon;
+
+    print('${llat}, ${llon}');
+  }
 
   // get address
+  /*
   void getLocationAddr() async {
     // 위도, 경도
     var lat = this.position.latitude;
@@ -57,6 +78,37 @@ class Location {
 
     print("주소 변환: $address");
   }
+*/
+  void getLocationAddr2() async {
+    // 위도, 경도
+    var lat = this.llat;
+    var lon = this.llon;
+
+    // kakao api: REST
+    Uri kakaoUri = Uri.parse(
+        "https://dapi.kakao.com/v2/local/geo/coord2address.json?x=$lon&y=$lat&input_coord=WGS84");
+    final String kakao_api = "48e1b9a10f747c23afbb5cbfd6de457c";
+
+    var kakaoAddr = await http
+        .get(kakaoUri, headers: {"Authorization": "KakaoAK $kakao_api"});
+    
+    // convert to json
+    var toJson = jsonDecode(kakaoAddr.body);
+
+    // parse json -> address
+    String addr = toJson["documents"][0]["address"]["region_1depth_name"] +
+        " " +
+        toJson["documents"][0]["address"]["region_2depth_name"] +
+        " " +
+        toJson["documents"][0]["address"]["region_3depth_name"];
+
+    // print(addr);
+    this.address = addr;
+
+    print("주소 변환: $address");
+  }
+
+
 
   // getters
   void getTempList() {
@@ -77,11 +129,48 @@ class Location {
     }
   }
 
+  void getSkyList() {
+    for(Weather w in skyList) {
+      w.getWeather();
+    }
+  }
   void getTmn() {
     print("최저 기온: $tmn");
   }
 
   void getTmx() {
     print("최고 기온: $tmx");
+  }
+
+  void weatherNow(var time) {
+    // List<String> weatherNow = new List.empty(growable: true);
+
+    for(Weather w in tempList) {
+      if(w.fcsttime == time) {
+        weatherNowList.add(w.fcstValue);
+        print("1");
+      }
+    }
+
+    for(Weather w in popList) {
+      if(w.fcsttime == time) {
+        weatherNowList.add(w.fcstValue);
+        print("2");
+      }
+    }
+
+    for(Weather w in rehList) {
+      if(w.fcsttime == time) {
+        weatherNowList.add(w.fcstValue);
+        print("3");
+      }
+    }
+
+    for(Weather w in skyList) {
+      if(w.fcsttime == time) {
+        weatherNowList.add(w.fcstValue);
+        print("4");
+      }
+    }
   }
 }
